46aae0efb7adaf4601d6aa637cff25a7f391e8b9
Nafiu <nafiu@pop-os.localdomain>
2022-11-24 09:23:16 +0700

    fix: adding flex on user name

--- app/features/home/screens/Home.tsx
+++ app/features/home/screens/Home.tsx
@@ -35,7 +35,7 @@ export default function Home({ navigation }: { navigation: CompositeNavigationPr
       <ImageBackground source={ require('~/assets/images/bg_home.png') } style={styles.container}>
         <View style={styles.container}>
           <View style={{ marginTop: 10, marginRight: 10, flexDirection: 'row', justifyContent: 'space-between' }}>
-            <View style={{ alignItems: 'center', marginLeft: 15, flexDirection: 'row' }}>
+            <View style={{ alignItems: 'center', marginLeft: 15, flexDirection: 'row', flex: 1 }}>
               <Text style={{ fontFamily: 'FredokaOne', fontSize: 20, color: bgcolor.blackUltraLight }}>
                 Halo, 
               </Text>
